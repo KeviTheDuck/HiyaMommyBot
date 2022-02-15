90b2e70caa498a51cad13c0fbee37b6fd712707d
Kinjal Kavish Boro <85004952+KeviTheDuck@users.noreply.github.com>
2022-02-15 15:41:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -120,8 +120,10 @@ def main():
     dp.add_error_handler(error_handler)
     updater.start_polling()
     updater.idle()
+
+keep_alive()
     
 if __name__ == '__main__':
     main()
 
-keep_alive()
+
